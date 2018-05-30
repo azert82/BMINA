8fa3215100467d24b832a0964009fb3c85414ba2
MehdiSoulimane <33414599+MehdiSoulimane@users.noreply.github.com>
2018-05-30 12:47:26 +0100

    Add files via upload

---
+++ lorann/view/src/main/java/view/Frame.java
@@ -0,0 +1,17 @@
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+	
+	private static final long serialversionVID = 1L;
+	
+	public Frame(string title)	{
+		setTitle(title);
+		setsize(700, 550);
+		setDefaultCloseOperation(Jframe.Exit_ON_CLOSE);
+		setResizable(True);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+
+}
---
+++ lorann/view/src/main/java/view/View.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JFrame;
+
+public class View {
+	SwingUtilities.invokeLater(new Runnable() {
+		public void run() {
+			new Frame ("Lorann");
+			}
+		}};
+	}
+}
+ 
