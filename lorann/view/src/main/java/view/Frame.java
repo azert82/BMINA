b94495e33364e5858422e641829aae3879bc2d6d
98bachir <39694764+98bachir@users.noreply.github.com>
2018-06-04 03:49:47 +0200

    connexion bdd

--- lorann/view/src/main/java/view/Frame.java
+++ lorann/view/src/main/java/view/Frame.java
@@ -13,5 +13,57 @@ public class Frame extends JFrame{
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
+	public class View extends JFrame {
+    public static void main(String[] args) throws SQLException {
+    	JFrame fenetre = new JFrame();
+        {
+        	JPanel pan = new JPanel();
+        fenetre.setTitle("LoraNN");
+        
+        fenetre.setSize(400, 400);
+       
+        fenetre.setLocationRelativeTo(null);
+        pan.setBackground(Color.black);
+        fenetre.setContentPane(pan);
+        fenetre.setVisible(true);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //fenetre.setContentPane(new panneau());
+        fenetre.setVisible(true);
+        
+        
+        }
+        String databaseURL = "jdbc:mysql://localhost:3306/testmap1?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String user = "root";
+        String password = "";
+        Connection conn = null;
+        
+        try {
+            conn = DriverManager.getConnection(databaseURL, user, password);
+            
+            if (conn != null) {
+                System.out.println("Connected to the database");
+            }
+        } 
+        catch (SQLException ex) {
+            System.out.println("An error occurred. Maybe user/password is invalid");
+            ex.printStackTrace();
+        } 
+        finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) 
+                {
+                    ex.printStackTrace();
+                }
+            }
+        }
+      //APPEL DE LA PROCEDURE 
+        String sql = "{call map1(?)}"; 
+        java.sql.CallableStatement call = conn.prepareCall(sql); 
+        call.executeQuery();
+        ResultSet resultat1 = call.getResultSet(); 
+        System.out.println(resultat1);
+        }
 
 }
